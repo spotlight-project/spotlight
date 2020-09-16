63293862ad3ae4c3618ee893b6b5281a962ac198
Sanne Kalkman <sannekalkman@gmail.com>
2020-09-16 13:20:43 +0200

    remove conflicting pipelines from router

--- lib/spotlight_web/router.ex
+++ lib/spotlight_web/router.ex
@@ -9,27 +9,4 @@ defmodule SpotlightWeb.Router do
       end
     end
   end
-
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_live_flash
-    plug :put_root_layout, {SpotlightWeb.LayoutView, :root}
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
-
-  scope "/", SpotlightWeb do
-    pipe_through :browser
-    live "/", PageLive, :index
-  end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", SpotlightWeb do
-  #   pipe_through :api
-  # end
 end
