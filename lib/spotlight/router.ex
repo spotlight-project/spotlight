1046286b2bc20a01a62d05a66aaf8ff7fecae706
Sanne Kalkman <sannekalkman@gmail.com>
2020-09-16 21:59:20 +0200

    move router to /spotlight

--- lib/spotlight/router.ex
+++ lib/spotlight/router.ex
@@ -1,11 +1,9 @@
-defmodule SpotlightWeb.Router do
-  use SpotlightWeb, :router
-
+defmodule Spotlight.Router do
   defmacro spotlight(path, opts \\ []) do
     quote bind_quoted: binding() do
       scope path, alias: false, as: false do
         import Phoenix.LiveView.Router, only: [live: 4]
-        live "/", SpotlightWeb.PageLive, :index
+        live "/", SpotlightWeb.PageLive, :index, opts
       end
     end
   end
